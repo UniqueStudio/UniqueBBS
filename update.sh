6d55442beeaf3ee68ec0f27ded11d9e1bc3771d9
ttzztztz <935319340@qq.com>
2019-06-16 15:47:00 +0800

    Remove useless files

--- update.sh
+++
@@ -1,2 +0,0 @@
-cd ~/BBS_Front/ && sudo docker cp ./ 57c161e37041:/var/www && sudo docker restart 57c161e37041
-cd ~/BBS_Back/ && sudo docker cp ./ f3d1541faf43:/usr/src/app/output && sudo docker restart f3d1541faf43
